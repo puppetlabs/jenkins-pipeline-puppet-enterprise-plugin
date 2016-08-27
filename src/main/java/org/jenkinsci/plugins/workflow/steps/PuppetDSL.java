cd45e3238561a999a09678c956a155caa0e11f83
Carl Caum <carl@carlcaum.com>
2016-08-26 19:48:27 -0700

    Provide dot syntax groovy methods
    This commit implements a groovy method called 'puppet' with two methods
    – 'puppet.codeDeploy' and 'puppet.run'

---
+++ src/main/java/org/jenkinsci/plugins/workflow/steps/PuppetDSL.java
@@ -0,0 +1,38 @@
+package org.jenkinsci.plugins.workflow.steps;
+
+import groovy.lang.Binding;
+import hudson.Extension;
+import java.io.IOException;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.ProxyWhitelist;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.StaticWhitelist;
+import org.jenkinsci.plugins.workflow.cps.CpsScript;
+import org.jenkinsci.plugins.workflow.cps.GlobalVariable;
+
+@Extension public class PuppetDSL extends GlobalVariable {
+
+  @Override public String getName() {
+    return "puppet";
+  }
+
+  @Override public Object getValue(CpsScript script) throws Exception {
+    Binding binding = script.getBinding();
+    Object puppet;
+    if (binding.hasVariable(getName())) {
+      puppet = binding.getVariable(getName());
+    } else {
+      // Note that if this were a method rather than a constructor, we would need to mark it @NonCPS lest it throw CpsCallableInvocation.
+      puppet = script.getClass().getClassLoader().loadClass("org.jenkinsci.plugins.workflow.Puppet").getConstructor(CpsScript.class).newInstance(script);
+      binding.setVariable(getName(), puppet);
+    }
+    return puppet;
+  }
+
+  @Extension public static class MiscWhitelist extends ProxyWhitelist {
+    public MiscWhitelist() throws IOException {
+      super(new StaticWhitelist(
+        "method groovy.lang.GroovyObject getProperty java.lang.String",
+        "method java.lang.Class isInstance java.lang.Object",
+        "method groovy.lang.GroovyObject invokeMethod java.lang.String java.lang.Object"));
+    }
+  }
+}
---
+++ src/main/resources/org/jenkinsci/plugins/workflow/Puppet.groovy
@@ -0,0 +1,81 @@
+package org.jenkinsci.plugins.workflow;
+
+class Puppet implements Serializable {
+
+  private org.jenkinsci.plugins.workflow.cps.CpsScript script
+
+  def credentialsId
+
+  public Puppet(org.jenkinsci.plugins.workflow.cps.CpsScript script) {
+    this.script = script
+  }
+
+  public <V> V credentials(String creds) {
+    credentialsId = creds
+  }
+
+  public <V> V codeDeploy(Map parameters = [:], String env) {
+    String credentials
+
+    if (parameters.credentials) {
+      credentials = parameters.credentials
+    } else {
+      credentials = credentialsId
+    }
+
+    if(credentials == null) {
+      System.out "No Credentials Provided for puppet.codeDeploy call"
+    }
+
+    node {
+      script.puppetCode(environment: env, credentialsId: credentialsId)
+    }
+  }
+
+  public <V> V run(Map parameters = [:], String env) {
+    String credentials
+    String target = null
+    Boolean noop = false
+    Integer concurrency = null
+
+    if (parameters.credentials) {
+      credentials = parameters.credentials
+    } else {
+      credentials = credentialsId
+    }
+
+    if (parameters.target) {
+      assert parameters.target instanceof String
+      target = parameters.target
+    }
+
+    if (parameters.noop) {
+      assert parameters.noop instanceof Boolean
+      noop = parameters.noop
+    }
+
+    if (parameters.concurrency) {
+      assert parameters.concurrency instanceof Integer
+      concurrency = parameters.concurrency
+    }
+
+    if(credentials == null) {
+      System.out "No Credentials Provided for puppet.run call"
+    }
+
+    node {
+      script.puppetJob(environment: env, target: target, concurrency: concurrency, credentialsId: credentials)
+    }
+  }
+
+  private <V> V node(Closure<V> body) {
+    if (script.env.NODE_NAME != null) {
+        // Already inside a node block.
+        body()
+    } else {
+        script.node {
+            body()
+        }
+    }
+  }
+}
