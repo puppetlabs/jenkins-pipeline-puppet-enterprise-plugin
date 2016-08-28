af2122f709392dbcedb39e4187ab535b06f312f0
Carl Caum <carl@carlcaum.com>
2016-08-27 18:52:45 -0700

    Enable the puppet.hiera function to lookup the job name

--- src/main/java/org/jenkinsci/plugins/workflow/steps/PuppetDSL.java
+++ src/main/java/org/jenkinsci/plugins/workflow/steps/PuppetDSL.java
@@ -32,6 +32,7 @@ import org.jenkinsci.plugins.workflow.cps.GlobalVariable;
       super(new StaticWhitelist(
         "method groovy.lang.GroovyObject getProperty java.lang.String",
         "method java.lang.Class isInstance java.lang.Object",
+        "staticMethod java.lang.System getenv java.lang.String",
         "method groovy.lang.GroovyObject invokeMethod java.lang.String java.lang.Object"));
     }
   }
